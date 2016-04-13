9f675c3ad2efa7062d073241576f3ffe4118afd5
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-04-13 11:03:53 -0300

    Mudança de nomenclaturas

--- estacionamento/estacionamento.c
+++ estacionamento/estacionamento.c
@@ -3,8 +3,8 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define CHAIRS 5
-#define NUM_CUST 25
+#define SLOTS 10
+#define NUM_CARS 100
 
 typedef int semaphore;
 
@@ -12,84 +12,86 @@ typedef struct {
     int idt;
 } thread_arg, *ptr_thread_arg;
 
-sem_t cust_sem, barb_sem, mutex;
+sem_t car_sem, plot_sem, mutex;
 int waiting = 0;
 int done = 0;
 int qtd = 0;
 
-void cut_hair() {
+void let_parking() {
 	//printf("Cortando cabelo...\n");
+	//fflush(stdin);
+}
+
+void park(int i) {
+	//printf("Carro %d estacionou por %d msecs...\n", i, i);
 	//fflush(stdin);
 	qtd++;
 }
 
-void get_haircut(int i) {
-	printf("Cliente %d recebendo corte...\n", i);
-	fflush(stdin);
-}
-
-void *barber_func(void *arg) {
+void *parking_lot_func(void *arg) {
 	while (!done) {
-		sem_wait(&cust_sem);
+		sem_wait(&car_sem);
 		sem_wait(&mutex);
 		
 		waiting--;
 		
-		sem_post(&barb_sem);
+		sem_post(&plot_sem);
 		sem_post(&mutex);
 
-		cut_hair();
+		let_parking();
 	}
 }
 
-void *customer_func(void *arg) {
+void *cars_func(void *arg) {
 	ptr_thread_arg t_arg = (ptr_thread_arg) arg;
 
 	sem_wait(&mutex);
 
-	printf("Cliente %d chegou!!!\n", t_arg->idt);
+	printf("Carro %d chegou!!!\n", t_arg->idt);
 
-	if (waiting < CHAIRS) {
+	if (waiting < SLOTS) {
 		waiting++;
-		printf("Cliente %d esperando!!!\n", t_arg->idt);
-		sem_post(&cust_sem);
-		sem_post(&mutex);
-		sem_wait(&barb_sem);
 
-		get_haircut(t_arg->idt);
+		printf("Carro %d estacionou por %d msecs...\n", t_arg->idt, t_arg->idt);
+		
+		sem_post(&car_sem);
+		sem_post(&mutex);
+		sem_wait(&plot_sem);
+
+		park(t_arg->idt);
 	} else {
 		sem_post(&mutex);
 	}
 
-	printf("Cliente %d foi embora!!!\n", t_arg->idt);
+	printf("Carro %d foi embora!!!\n", t_arg->idt);
 }
 
 int main() {
 	int i;
-	pthread_t barber;
-	pthread_t customers[NUM_CUST];
-	thread_arg cust_args[NUM_CUST];
+	pthread_t parking_lot;
+	pthread_t cars[NUM_CARS];
+	thread_arg car_args[NUM_CARS];
 
-	sem_init(&cust_sem, 0, 0);
-	sem_init(&barb_sem, 0, 0);
+	sem_init(&car_sem, 0, 5);
+	sem_init(&plot_sem, 0, 0);
 	sem_init(&mutex, 0, 1);
 
-	pthread_create(&barber, NULL, barber_func, NULL);
+	pthread_create(&parking_lot, NULL, parking_lot_func, NULL);
 
-	for (i = 0; i < NUM_CUST; i++) {
-        cust_args[i].idt = i;
+	for (i = 0; i < NUM_CARS; i++) {
+        car_args[i].idt = i;
 
-        pthread_create(&(customers[i]), NULL, customer_func, &(cust_args[i]));
+        pthread_create(&(cars[i]), NULL, cars_func, &(car_args[i]));
     }
 
-    for (i = 0; i < NUM_CUST; i++) {
-    	pthread_join(customers[i], NULL);
+    for (i = 0; i < NUM_CARS; i++) {
+    	pthread_join(cars[i], NULL);
     }
 
     done = 1;
 
-	sem_destroy(&cust_sem);
-	sem_destroy(&barb_sem);
+	sem_destroy(&car_sem);
+	sem_destroy(&plot_sem);
 	sem_destroy(&mutex);
 
 	printf("qtd = %d\n", qtd);
